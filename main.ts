3db1911f9bd670e53647d5d3f7a7d30764002ebc
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-11-17 11:29:24 +0900

    smcc jwt

---
+++ main.ts
@@ -0,0 +1,6 @@
+import jwt_decode from "jwt-decode";
+
+var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+var decoded = jwt_decode(token);
+
+console.log(decoded);
